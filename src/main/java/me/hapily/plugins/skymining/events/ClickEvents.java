4404f62279f4d7392e4120b2a722a00171775279
hapily04 <87914807+hapily04@users.noreply.github.com>
2022-01-08 15:29:14 -0600

    fixed an issue where the event was being called for both hands

--- src/main/java/me/hapily/plugins/skymining/events/ClickEvents.java
+++ src/main/java/me/hapily/plugins/skymining/events/ClickEvents.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 import java.util.Objects;
 
@@ -15,29 +16,32 @@ public class ClickEvents implements Listener {
     @EventHandler
     public void onClick(PlayerInteractEvent event){
         Player p = event.getPlayer();
-        if(p.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase("§5§lMines Selection Tool")){
-            event.setCancelled(true);
-            if(event.getAction().isLeftClick()){
-                if(Selection.loc1.containsKey(p)){
-                    Selection.loc1.replace(p, Objects.requireNonNull(event.getClickedBlock()).getLocation());
+        if (event.getHand() == EquipmentSlot.HAND){
+            if(p.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase("§5§lMines Selection Tool")){
+                event.setCancelled(true);
+                if(event.getAction().isLeftClick()){
+                    if(Selection.loc1.containsKey(p)){
+                        Selection.loc1.replace(p, Objects.requireNonNull(event.getClickedBlock()).getLocation());
+                    }
+                    else{
+                        Selection.loc1.put(p, Objects.requireNonNull(event.getClickedBlock()).getLocation());
+                    }
+                    Location loc = event.getClickedBlock().getLocation();
+                    p.sendMessage("§5Mines: §dFirst position set to (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ").");
                 }
-                else{
-                    Selection.loc1.put(p, Objects.requireNonNull(event.getClickedBlock()).getLocation());
+                else if(event.getAction().isRightClick()){
+                    if(Selection.loc2.containsKey(p)){
+                        Selection.loc2.replace(p, Objects.requireNonNull(event.getClickedBlock()).getLocation());
+                    }
+                    else{
+                        Selection.loc2.put(p, Objects.requireNonNull(event.getClickedBlock()).getLocation());
+                    }
+                    Location loc = event.getClickedBlock().getLocation();
+                    p.sendMessage("§5Mines: §dSecond position set to (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ").");
                 }
-                Location loc = event.getClickedBlock().getLocation();
-                p.sendMessage("§5Mines: §dFirst position set to (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ").");
-            }
-            else if(event.getAction().isRightClick()){
-                if(Selection.loc2.containsKey(p)){
-                    Selection.loc2.replace(p, Objects.requireNonNull(event.getClickedBlock()).getLocation());
-                }
-                else{
-                    Selection.loc2.put(p, Objects.requireNonNull(event.getClickedBlock()).getLocation());
-                }
-                Location loc = event.getClickedBlock().getLocation();
-                p.sendMessage("§5Mines: §dSecond position set to (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ").");
             }
         }
+
 
     }
 
